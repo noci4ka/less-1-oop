43e288ba4377573883c0635d18627069ac40229c
Student <student@ter.itstep>
2024-12-07 14:51:22 +0200

    commit name

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 class Student:
-    def __init__(self, name):
+    def __init__(self, name, age):
         self.name = name
         self.age = age
-    def greeting(self):
-        print(f'hello i am {self.name} student')
 
-maxym_student = Student(name = 'maxym' )
-maxym_student.greeting
+    def greeting(self):
+        print(f"hello I am {self.name}")
+
+    def grow_up(self):
+        self.age = self.age + 1
+    
+    def print_age(self):
+        print(f"age - {self.age}")
+
+
+maxym_student = Student(name = "Maxym", age=12)
+maxym_student.greeting()
+
+matvii_stydent = Student(name = "Matvii", age=14)
+matvii_stydent.greeting()
+
+matvii_stydent.print_age()
+matvii_stydent.grow_up()
+matvii_stydent.print_age()
+
+maxym_student.print_age()
