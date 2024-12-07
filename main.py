9372705d49cf18fad1eb1b6a7f11a912372ce750
Student <student@ter.itstep>
2024-12-07 14:45:58 +0200

    commit name

---
+++ main.py
@@ -0,0 +1,9 @@
+class Student:
+    def __init__(self, name):
+        self.name = name
+        self.age = age
+    def greeting(self):
+        print(f'hello i am {self.name} student')
+
+maxym_student = Student(name = 'maxym' )
+maxym_student.greeting
